3c712975d750e86affcd67843009ec657e3f90f4
Owain Bates <owainbates@rocketmail.com>
2014-08-29 19:04:17 +0100

    Added exceptions for keyboardinterrupts
    This means that generation of lists can be stopped at any time, and the program will proceed to the next step using already obtained numbers rather than ending abruptly. This could be useful if you accidentally made the range 0-1000 but at 250 decided you wanted to stop. This can be done for every list generation stage. Obviously the overall range will change depending on the time at which you stopped.

--- Palprimer.py
+++ Palprimer.py
@@ -6,45 +6,68 @@ palprimeslist = []
 hpalprimeslist = []
 print("PRIMES FOR THAT RANGE:")
 time.sleep(1)
-for i in range (bottomrange,toprange):
-    num = i
-    for i in range(2,int(math.sqrt(num))):
-       if (num % i) == 0:
-           break
-    else:
-        print(num)
-        primeslist.append(num)
+try:
+    for i in range (bottomrange,toprange):
+        num = i
+        for i in range(2,int(math.sqrt(num))):
+           if (num % i) == 0:
+               break
+        else:
+            print(num)
+            primeslist.append(num)
+except KeyboardInterrupt:
+    time.sleep(1)
+    print()
+    print("HALTED. ALL FURTHER LISTS WILL ONLY INCLUDE NUMBER ABOVE.")
+    time.sleep(1)
 print("THAT'S A TOTAL OF:")
 time.sleep(1)
 print(len(primeslist))
 time.sleep(1)
 print("OF THOSE, THESE ARE PALPRIMES:")
 time.sleep(1)
-for item in primeslist:
-    if str(item) != str(item)[::-1] or item<10:
-        pass
-    else:
-        print(item)
-        palprimeslist.append(item)
-time.sleep(1)
+try:
+    for item in primeslist:
+        if str(item) != str(item)[::-1] or item<10:
+            pass
+        else:
+            print(item)
+            palprimeslist.append(item)
+except KeyboardInterrupt:
+    print()
+    time.sleep(1)
+    print("HALTED. ALL FURTHER LISTS WILL ONLY INCLUDE NUMBER ABOVE.")
+    time.sleep(1)        
 print("THAT'S A TOTAL OF:")
 time.sleep(1)
 print(len(palprimeslist))
 time.sleep(1)
 print("NOW HAPPY PALPRIMES:")
-time.sleep(1)
-def digits(n):
-  if n == 0:
-    return []
-  else:
-    return digits(n // 10) + [n % 10]
-digit_squares = [n*n for n in range(0, 10)]
-end_loop = [4, 16, 37, 58, 89, 145, 42, 20]
-for item in palprimeslist:
-  i = item
-  while i not in [1] + end_loop:
-    i = sum([digit_squares[d] for d in digits(i)])
-  if i == 1:
-    print(item)
+try:
+    time.sleep(1)
+    def digits(n):
+      if n == 0:
+        return []
+      else:
+        return digits(n // 10) + [n % 10]
+    digit_squares = [n*n for n in range(0, 10)]
+    end_loop = [4, 16, 37, 58, 89, 145, 42, 20]
+    for item in palprimeslist:
+      i = item
+      while i not in [1] + end_loop:
+        i = sum([digit_squares[d] for d in digits(i)])
+      if i == 1:
+        print(item)
+except KeyboardInterrupt:
+    time.sleep(1)
+    print()
+    print("HALTED")
 time.sleep(1)              
 input("Press to exit")
+       
+       
+   
+
+       
+       
+   
